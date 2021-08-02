ce0c82695ea7e1cb2262ad2bc2ee14209f372b5e
Timi-cell <adedayotimilehin10@gmail.com>
2021-08-02 19:01:54 +0100

    completed the contact page and its responsiveness

--- script.js
+++ script.js
@@ -23,38 +23,40 @@
 
 // showSlides();
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
+//MANUAL SLIDESHOW
+ var slideIndex = 1;
+ showSlides(slideIndex);
 //next and prev controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+ function plusSlides(n) {
+   showSlides((slideIndex += n));
+ }
 
 //thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+ function currentSlide(n) {
+   showSlides((slideIndex = n));
+ }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("container-slider");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("container-slider");
+   var dots = document.getElementsByClassName("dot");
+   if (n > slides.length) {
+     slideIndex = 1;
+   }
+   if (n < 1) {
+     slideIndex = slides.length;
+   }
+   for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " active";
+ }
+
+
 // open
 var menu = document.getElementById("dashes");
 menu.addEventListener("click", openNav);
